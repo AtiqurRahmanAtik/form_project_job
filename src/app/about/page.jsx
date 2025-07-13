b8c638f295521a67c4540924f409b0bdfe0705f7
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2025-07-13 17:09:30 +0600

    added Static metadata for all pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,11 @@
 
 import React from 'react';
 
+export const metadata = {
+  title: 'About Us',
+  description: 'Learn more about our company and values.',
+};
+ 
 
 const About = () => {
 
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const metadata = {
+  title: 'Contact Us',
+  description: 'Get in touch with us for any inquiries or support.',
+};
+
 const Contacts = () => {
     return (
         <div>
